3aec09534d666b71f0e809c10f87904537c108f9
Ata Türkoğlu <ata.turkoglu@reengen.com>
2024-06-21 16:17:36 +0300

    add new transitions

--- src/client/components/introVideo.jsx
+++ src/client/components/introVideo.jsx
@@ -1,30 +1,43 @@
 import React from "react";
-import TextTransitions from "./textTransitions";
+import TextTransitions, { TextContainer } from "./textTransitions";
+import "./css/introVideo.css";
 
 export default function IntroVideo() {
     return (
-        <div>
-            <div className="h-full overflow-hidden hidden md:block">
+        <div className="relative">
+            <div className="h-full w-full overflow-hidden hidden md:block">
                 <video
                     autoPlay={true}
                     muted
                     style={{
                         filter: "brightness(50%)",
+                        zIndex: 0,
                     }}
                 >
                     <source src="/assets/video/pmzaltin.mp4" type="video/mp4" />
                 </video>
-                <TextTransitions />
             </div>
-            <div className="h-screen md:hidden">
+            <div className="h-screen w-full md:hidden">
                 <img
-                    className="w-full h-full object-cover"
+                    className="w-full h-full object-cover slide"
                     src="/assets/common/view.jpg"
                     style={{
                         filter: "brightness(70%)",
                         objectPosition: "40%",
                     }}
                 />
+            </div>
+            <div className="absolute z-10 bottom-1/4 inset-x-0 h-fit w-full px-4">
+                <TextTransitions>
+                    <TextContainer>Sıfır atık ile çalışıyoruz...</TextContainer>
+                    <TextContainer>
+                        Türkiyenin ilk ve tek Rutil üreticisiyiz.
+                    </TextContainer>
+                    <TextContainer>Doğaya saygıyla...</TextContainer>
+                    <TextContainer>
+                        Ülkenin ilk ve tek Garnet üreticisiyiz.
+                    </TextContainer>
+                </TextTransitions>
             </div>
         </div>
     );
--- src/client/components/textTransitions.jsx
+++ src/client/components/textTransitions.jsx
@@ -1,10 +1,75 @@
-import React, { useLayoutEffect, useEffect, useState, useRef } from "react";
+import React, {
+    useLayoutEffect,
+    useEffect,
+    useState,
+    useRef,
+    cloneElement,
+} from "react";
 
-export function textContainer({ children }) {
-    return <span>{children}</span>;
+import "./css/textTransition.css";
+
+export function TextContainer({ children }) {
+    return children;
 }
 
-export default function TextTransitions() {
+export default function TextTransitions({ children }) {
     const [content, setContent] = useState(null);
-    return <div className="h-fit">{content}</div>;
+
+    const setWords = (item) => {
+        return item.props.children.split(" ").map((el, idx) => {
+            return (
+                <span
+                    key={idx}
+                    className="mr-2 text-white text-2xl md:text-5xl"
+                >
+                    {el}
+                </span>
+            );
+        });
+    };
+
+    useLayoutEffect(() => {
+        const list = children.map((item, index) => {
+            return (
+                <h1
+                    id={"text" + index}
+                    key={index}
+                    className={index == 0 ? "" : "hidden"}
+                >
+                    {setWords(item)}
+                </h1>
+            );
+        });
+        setContent(list);
+
+        let counter = 0;
+
+        const showIntrvl = setInterval(() => {
+            if (counter != 0) {
+                document.getElementById("text" + (counter - 1)).style.display =
+                    "none";
+            } else {
+                const prevEl = document.getElementById(
+                    "text" + (children.length - 1)
+                );
+                if (prevEl.style.display == "block") {
+                    prevEl.style.display = "none";
+                }
+            }
+
+            document.getElementById("text" + counter).style.display = "block";
+
+            if (counter < children.length - 1) {
+                counter++;
+            } else {
+                counter = 0;
+            }
+        }, 5000);
+
+        return () => {
+            clearInterval(showIntrvl);
+        };
+    }, []);
+
+    return <div className="h-100 w-full">{content}</div>;
 }
