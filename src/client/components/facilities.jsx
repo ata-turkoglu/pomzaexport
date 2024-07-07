7a6096657e3949d528e150650bc9d8891f052ce1
Ata Türkoğlu <ata.turkoglu@reengen.com>
2024-07-08 00:05:02 +0300

    add new style

--- src/client/components/facilities.jsx
+++ src/client/components/facilities.jsx
@@ -5,10 +5,12 @@ import { useNavigate } from "react-router-dom";
 export default function Facilities() {
     const navigate = useNavigate();
     return (
-        <div className="w-full h-fit flex flex-col py-10 md:pt-20 md:pb-28 md:pl-10 md:pr-5">
-            <div className="w-full">
-                <h2 className="text-2xl md:text-4xl mb-2 md:mb-5 ml-3 md:ml-0">
-                    İşletmelerimizi İnceleyin
+        <div className="bg-[#151a381a] relative w-full h-fit flex flex-col pt-10 md:pt-20 md:pb-10 md:pl-10 md:pr-5">
+            <div className="w-full h-fit flex">
+                <h2 className="text-xl md:text-4xl mb-2 md:mb-5 ml-3 md:ml-0 w-full md:w-1/3 z-30 bg-transparent">
+                    Birçok farklı sektöre <br></br> ilgili kalite standartları
+                    ile <br></br>hammaddeler ürettiğimiz <br></br>
+                    işletmelerimizi ve ürünlerimizi inceleyin
                 </h2>
             </div>
             <div className="w-full h-84 flex flex-col md:flex-row items-center justify-center">
